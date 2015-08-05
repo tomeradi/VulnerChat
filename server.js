fa7bbfcc3500ecf7fd0e53a7d33b67e8a206489f
Mark Nunnikhoven <me@markn.ca>
2015-08-05 16:23:44 -0400

    Cleaned up the UX for users joining the chat

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var io = require('socket.io')(http);
 
 // message queue
 var msg_queue = [];
+var msg_queue_limit = 15;
+var users = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/html/index.html');
@@ -17,16 +19,26 @@ io.on('connection', function(socket){
 
   socket.on('user_login', function(user_name) {
   	console.log('new user: ' + user_name);
-  	// return the chat history to this user
-  	io.emit(user_name + ' joined the chat');
+  	// announce the user 
+  	io.emit('chat_message', user_name + ' joined the chat');
+
+  	users[user_name] = socket.id; 	
+  	// return the latest messages from the chat history to this user
+  	for	(i = 0; i < msg_queue.length; i++) {
+  		io.to(users[user_name]).emit('chat_message', msg_queue[i]);
+  	}
   });
 
   socket.on('disconnect', function() {
-    console.log('user disconnected');
+    console.log('disconnect');
   });
 
   socket.on('chat_message', function(msg){
     console.log(msg.user + ' says: ' + msg.message);
+    msg_queue.push(msg);
+    if (msg_queue.length == msg_queue_limit) {
+    	msg_queue.shift();
+    }
     io.emit('chat_message', msg);
   });
 });
