4bfa7ba4ab2594476dead2dad6301b51d1dcd582
Mark Nunnikhoven <me@markn.ca>
2015-08-05 16:13:05 -0400

    Basic working prototype that allows XSS attacks

--- server.js
+++ server.js
@@ -2,18 +2,32 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// message queue
+var msg_queue = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/html/index.html');
 });
 
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/html/chat.html');
+});
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
+
+  socket.on('user_login', function(user_name) {
+  	console.log('new user: ' + user_name);
+  	// return the chat history to this user
+  	io.emit(user_name + ' joined the chat');
+  });
+
+  socket.on('disconnect', function() {
     console.log('user disconnected');
   });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+
+  socket.on('chat_message', function(msg){
+    console.log(msg.user + ' says: ' + msg.message);
+    io.emit('chat_message', msg);
   });
 });
 
