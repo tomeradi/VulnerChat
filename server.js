1855554b4a063a5da597cc27318302987c7f9b55
Mark Nunnikhoven <me@markn.ca>
2015-08-05 21:32:57 -0400

    Added core function for sending messages to clients

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ var io = require('socket.io')(http);
 // message queue
 var msg_queue = [];
 var msg_queue_limit = 15;
+
+// user info & db
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('leakychat.db'); // :memory: also an option
 var users = {};
+
+// setup the db table if it isn't already
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS messages (user_name TEXT, timestamp TEXT, body TEXT)");
+});
+ 
+db.close();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/html/index.html');
@@ -15,12 +26,17 @@ app.get('/chat', function(req, res){
   res.sendFile(__dirname + '/html/chat.html');
 });
 
+function send_message(message) {
+	io.emit('chat_message', message);
+}
+
 io.on('connection', function(socket){
 
   socket.on('user_login', function(user_name) {
   	console.log('new user: ' + user_name);
   	// announce the user 
-  	io.emit('chat_message', { 'user': 'Leakybot', 'message': user_name + ' joined the chat' });
+  	//io.emit('chat_message', { 'user': 'Leakybot', 'message': user_name + ' joined the chat' });
+  	send_message({ 'user': 'Leakybot', 'message': user_name + ' joined the chat' });
 
   	users[user_name] = socket.id; 	
   	// return the latest messages from the chat history to this user
@@ -36,10 +52,12 @@ io.on('connection', function(socket){
   socket.on('chat_message', function(msg){
     console.log(msg.user + ' says: ' + msg.message);
     msg_queue.push(msg);
+
+    // make sure we keep the queue a reasonable length
     if (msg_queue.length == msg_queue_limit) {
     	msg_queue.shift();
     }
-    io.emit('chat_message', msg);
+    send_message(msg);
   });
 });
 
